7fd4fe21807a9fe94f5dd54096b2762a81b3aba4
Paul Hansen <paul@chunk.Stanford.EDU>
2017-08-13 16:41:56 -0700

    WIP privatized histograms

--- histogram_privatized.cu
+++ histogram_privatized.cu
@@ -6,12 +6,7 @@
 
 __global__
 void histogram_privatized_kernel(int arrayLength, const float* x, int numBins, float firstEdge, float lastEdge, int* outHist)
-{
-    // Approaches:
-    // 1. One thread per element, writing into a single histogram.  I can figure this out now.
-    // 2. One thread per bin, reading the entire array.  I can figure this out now.
-    // 3. Something else
-    
+{   
     // histogram_thread_bin_kernel: each thread handles one bin
     
     int idx = blockIdx.x*blockDim.x + threadIdx.x;
@@ -49,13 +44,25 @@ void histogram_privatized_kernel(int arrayLength, const float* x, int numBins, f
 void histogram_privatized(int arrayLength, const float* x, int numBins, float firstEdge, float lastEdge, int* outHist, int gridSize, int blockSize)
 {
     float* d_x;
-    int* d_histogram;
+    int* d_histograms;
+    
+    // We want each thread to have its own histogram.  I can stuff them all into one
+    // histogram array.  Each thread is also responsible for a certain number of array
+    // elements...
+    
+    numThreads = gridSize*blockSize;
+    elemsPerThread = arrayLength/numThreads;
+    
+    I had gotten this far.  Really you should think through the whole algorithm from scratch.
     
     checkCudaErrors(cudaMalloc(&d_x, arrayLength*sizeof(float)));
-    checkCudaErrors(cudaMalloc(&d_histogram, numBins*sizeof(int)));
+    checkCudaErrors(cudaMalloc(&d_histogram, numBins*numThreads*sizeof(int)));
     
     checkCudaErrors(cudaMemcpy(d_x, x, arrayLength*sizeof(float), cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMemset(d_histogram, 0, numBins*sizeof(int)));
+    checkCudaErrors(cudaMemset(d_histogram, 0, numBins*numThreads*sizeof(int)));
+    
+    // In this method we have each thread run a sub-histogram and then combine.
+    // Each thread needs its own memory.  Hmm.
     
     histogram_privatized_kernel<<<gridSize, blockSize>>>(arrayLength, d_x, numBins, firstEdge, lastEdge, d_histogram);
     checkCudaErrors(cudaMemcpy(outHist, d_histogram, numBins*sizeof(float), cudaMemcpyDeviceToHost));
